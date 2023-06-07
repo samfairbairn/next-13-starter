fcdee50b0ab58ca6743f21afe3b9fabb0b1afa0b
samfairbairn <samfairbairn@hotmail.co.uk>
2023-06-07 14:27:57 +0800

    add transparent video

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -4,8 +4,15 @@ export default function About() {
   return (
     <main className="flex flex-col min-h-screen w-screen">
       <div className="flex items-center justify-center w-screen h-screen">
-        <h2 className={`type-title`}>About</h2>
+        <div className="flex items-center justify-center w-screen max-w-5xl mx-auto rounded-3xl bg-gradient-to-b from-blue to-purple">
+          <video preload="metadata" autoPlay muted playsInline loop>
+            {/* <source src="/videos/movie-mp4.mp4" type={'video/mp4; codecs="hvc1"'} /> */}
+            <source src="/videos/movie-mov.mov" type='video/quicktime; codecs="hvc1"' />
+            <source src="/videos/movie-webm.webm" type="video/webm" />
+          </video>
+        </div>
       </div>
+
       <div className="flex items-center justify-center w-screen h-screen">
         <h2 className={`type-title`}>About</h2>
       </div>
